08de0a0bacf17a36a87fbb397052211e5778b0f1
Amirhossein Shariatikia <amirshk76@outlook.com>
2020-09-30 14:22:21 +0330

    push change

--- js/controller.js
+++ js/controller.js
@@ -245,13 +245,37 @@ routeAppControllers.controller('topologyController', function($scope, $document,
     }
     csvString = csvString.substring(0, csvString.length - 1);
     console.log(csvString);
-    var a = $('<a/>', {
-    style:'display:none',
-    href:'data:application/octet-stream;base64,'+btoa(csvString),
-    download:'emailStatistics.csv'
-    }).appendTo('body')
-    a[0].click();
+    saveTextAsFile(csvString , "test.csv");
   };  
+
+
+ function saveTextAsFile (data, filename){
+
+        if(!data) {
+            console.error('Console.save: No data')
+            return;
+        }
+
+        if(!filename) filename = 'console.json'
+
+        var blob = new Blob([data], {type: 'text/plain'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a')
+  if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(blob, filename);
+  }
+  else{
+      var e = document.createEvent('MouseEvents'),
+          a = document.createElement('a');
+
+      a.download = filename;
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+      e.initEvent('click', true, false, window,
+          0, 0, 0, 0, 0, false, false, false, false, 0, null);
+      a.dispatchEvent(e);
+  }
+};
 
   $scope.getrecords = function(){
     return records;  
